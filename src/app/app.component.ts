76379ca6d9937e67b4284b6a8b6c9786b96c88fb
vinudasenith <ha.vinudas@gmail.com>
2025-07-05 18:37:52 +0530

    implement navigation bar👍

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { Router, NavigationEnd, RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { CommonModule } from '@angular/common';
+import { filter } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, NavbarComponent, CommonModule],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'hotel-booking-frontend';
+  isLoginPageOrRegisterPage: boolean = false;
+
+  constructor(private router: Router) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: any) => {
+      const path = event.urlAfterRedirects;
+      this.isLoginPageOrRegisterPage = path === '/login' || path === '/register';
+    });
+  }
+}
---
+++ src/app/app.config.ts
@@ -0,0 +1,12 @@
+import { importProvidersFrom } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { routes } from './app.routes';
+
+export const appConfig = {
+  providers: [
+    importProvidersFrom(RouterModule),
+    provideRouter(routes),
+
+  ]
+};
