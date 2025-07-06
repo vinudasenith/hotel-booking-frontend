910f634772ba3984002b8877a542a6bc0ab7e7f7
vinudasenith <ha.vinudas@gmail.com>
2025-07-06 21:08:31 +0530

    chore(admin-rooms): verify routing setup👍

---
+++ src/app/pages/admin-rooms/admin-rooms.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-admin-rooms',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './admin-rooms.component.html',
+  styleUrl: './admin-rooms.component.css'
+})
+export class AdminRoomsComponent {
+
+}
