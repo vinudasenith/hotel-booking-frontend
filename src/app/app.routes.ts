ffe7cf1b3f995f598f0aeb691d48f4a95cfe6e4f
vinudasenith <ha.vinudas@gmail.com>
2025-07-06 21:16:31 +0530

    add Gallery,Admin pages and  Spa routes with conditional layout handling in app component👍

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,24 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { HomeComponent } from './pages/home/home.component';
-
+import { AdminComponent } from './pages/admin/admin.component';
+import { AdminRoomsComponent } from './pages/admin-rooms/admin-rooms.component';
+import { AdminUsersComponent } from './pages/admin-users/admin-users.component';
+import { AdminBookingsComponent } from './pages/admin-bookings/admin-bookings.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { SpaComponent } from './pages/spa/spa.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: 'home', component: HomeComponent },
-]
+    { path: 'admin', component: AdminComponent },
+    { path: 'admin/rooms', component: AdminRoomsComponent },
+    { path: 'admin/users', component: AdminUsersComponent },
+    { path: 'admin/bookings', component: AdminBookingsComponent },
+    { path: 'gallery', component: GalleryComponent },
+    { path: 'spa', component: SpaComponent }
+
+
+];
