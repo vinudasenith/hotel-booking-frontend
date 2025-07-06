072e8c05b32461a05efa45d2f029d0f6478f0d62
vinudasenith <ha.vinudas@gmail.com>
2025-07-06 16:11:39 +0530

    feat: add basic routing with redirect to home👍

---
+++ src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { HomeComponent } from './pages/home/home.component';
+
+
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'home', component: HomeComponent },
+]
