15dc79cce84787a985b142d90cfbf29daf2b4798
vinudasenith <ha.vinudas@gmail.com>
2025-07-06 21:06:40 +0530

    design admin dashboard with navigation cards👍

---
+++ src/app/pages/admin/admin.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-admin',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './admin.component.html',
+  styleUrl: './admin.component.css'
+})
+export class AdminComponent {
+
+}
