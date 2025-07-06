dc49c5fb9b5be980334e84e2703b48fe81d8682b
vinudasenith <ha.vinudas@gmail.com>
2025-07-06 21:08:07 +0530

    chore(admin-bookings): verify routing setup👍

---
+++ src/app/pages/admin-bookings/admin-bookings.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-bookings',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './admin-bookings.component.html',
+  styleUrl: './admin-bookings.component.css'
+})
+export class AdminBookingsComponent {
+
+}
