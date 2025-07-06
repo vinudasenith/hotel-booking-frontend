a8fc2cd16f00835008a34021fc049bd76086db87
vinudasenith <ha.vinudas@gmail.com>
2025-07-06 21:13:49 +0530

    implement full spa page with banner, treatments, and gallery👍

---
+++ src/app/pages/spa/spa.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-spa',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './spa.component.html',
+  styleUrl: './spa.component.css'
+})
+export class SpaComponent {
+
+}
