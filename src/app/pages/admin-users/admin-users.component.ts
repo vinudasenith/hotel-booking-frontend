f86b02e1bb9e45136d09afdc88577abc2a759cb5
vinudasenith <ha.vinudas@gmail.com>
2025-07-06 21:08:52 +0530

    chore(admin-users): verify routing setup👍

---
+++ src/app/pages/admin-users/admin-users.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-admin-users',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './admin-users.component.html',
+  styleUrl: './admin-users.component.css'
+})
+export class AdminUsersComponent {
+
+}
