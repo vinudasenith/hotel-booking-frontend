a7fdaa0faef82ee001f79a8427a509cc7bcc81d4
vinudasenith <ha.vinudas@gmail.com>
2025-07-07 21:08:52 +0530

    feat: add styled FAQ page with categorized booking, amenities, and services sections 👍

---
+++ src/app/pages/faqs/faqs.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-faqs',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './faqs.component.html',
+  styleUrl: './faqs.component.css'
+})
+export class FaqsComponent {
+
+}
