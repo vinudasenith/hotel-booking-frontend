38845ae443b6d0580f83fc429ae308a2e95d0d27
vinudasenith <ha.vinudas@gmail.com>
2025-07-07 21:11:05 +0530

    feat: add routing for about, contact, faqs, terms, things to do, and facilities pages👍

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,6 +8,12 @@ import { AdminUsersComponent } from './pages/admin-users/admin-users.component';
 import { AdminBookingsComponent } from './pages/admin-bookings/admin-bookings.component';
 import { GalleryComponent } from './pages/gallery/gallery.component';
 import { SpaComponent } from './pages/spa/spa.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import { FaqsComponent } from './pages/faqs/faqs.component';
+import { TermsComponent } from './pages/terms/terms.component';
+import { ThingstodoComponent } from './pages/thingstodo/thingstodo.component';
+import { FacilitiesComponent } from './pages/facilities/facilities.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -19,7 +25,13 @@ export const routes: Routes = [
     { path: 'admin/users', component: AdminUsersComponent },
     { path: 'admin/bookings', component: AdminBookingsComponent },
     { path: 'gallery', component: GalleryComponent },
-    { path: 'spa', component: SpaComponent }
+    { path: 'spa', component: SpaComponent },
+    { path: 'about', component: AboutUsComponent },
+    { path: 'contact', component: ContactUsComponent },
+    { path: 'faqs', component: FaqsComponent },
+    { path: 'terms', component: TermsComponent },
+    { path: 'thingstodo', component: ThingstodoComponent },
+    { path: 'facilities', component: FacilitiesComponent },
 
 
 ];
