a2eed29e715cb0f0a5dd7a25d47ea375bc6c6ef7
vinudasenith <ha.vinudas@gmail.com>
2025-07-07 21:08:13 +0530

    feat: implement Terms and Conditions page with full layout and section styling👍

---
+++ src/app/pages/terms/terms.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-terms',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './terms.component.html',
+  styleUrl: './terms.component.css'
+})
+export class TermsComponent {
+
+}
