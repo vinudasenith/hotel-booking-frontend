2db837692c5e9325eddf0bf424dca290151ce53a
vinudasenith <ha.vinudas@gmail.com>
2025-07-07 21:09:59 +0530

    feat: implement facilities page with spa, pool, gym, business center, and services 👍

---
+++ src/app/pages/facilities/facilities.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-facilities',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './facilities.component.html',
+  styleUrl: './facilities.component.css'
+})
+export class FacilitiesComponent {
+
+}
