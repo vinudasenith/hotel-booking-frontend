a2e5a6d61081a194a39626c79e499b62a15cb7d1
vinudasenith <ha.vinudas@gmail.com>
2025-07-11 20:22:53 +0530

    fetch and display users in admin panel👍

--- src/app/pages/admin-users/admin-users.component.ts
+++ src/app/pages/admin-users/admin-users.component.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-admin-users',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './admin-users.component.html',
-  styleUrl: './admin-users.component.css'
-})
-export class AdminUsersComponent {
+  styleUrls: ['./admin-users.component.css']
 
+})
+
+export class AdminUsersComponent implements OnInit {
+
+  users: any[] = [];
+  loading: boolean = true;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.fetchUsers();
+  }
+
+  fetchUsers() {
+    this.http.get<any[]>('http://localhost:8080/api/users/all').subscribe({
+      next: (data) => {
+        this.users = data;
+        this.loading = false;
+      },
+      error: (err) => {
+        console.error('Failed to fetch users:', err);
+        this.loading = false;
+      }
+    });
+  }
+
+  handleBlockUser(email: string) {
+    console.log('Block/unblock user:', email);
+  }
 }
