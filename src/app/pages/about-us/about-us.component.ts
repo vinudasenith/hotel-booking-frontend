34e5e5c9d60a909a2d7f984329e223388e24c2f4
vinudasenith <ha.vinudas@gmail.com>
2025-07-13 21:25:47 +0530

    update router link✅

--- src/app/pages/about-us/about-us.component.ts
+++ src/app/pages/about-us/about-us.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-about-us',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.css'
 })
