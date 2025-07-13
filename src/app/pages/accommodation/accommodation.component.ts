2731c9ad8962f7185f92f6a93f1b3a77ce303188
vinudasenith <ha.vinudas@gmail.com>
2025-07-13 21:27:09 +0530

    add responsive layout and design👍

---
+++ src/app/pages/accommodation/accommodation.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accommodation',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './accommodation.component.html',
+  styleUrl: './accommodation.component.css'
+})
+export class AccommodationComponent {
+
+}
