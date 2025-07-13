9b32f5d4bff3a174c15bff28ee625254b2efc731
vinudasenith <ha.vinudas@gmail.com>
2025-07-13 21:24:59 +0530

    update content✅

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,39 +1,17 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 
 
 export class HomeComponent {
-  startDate: string = '';
-  endDate: string = '';
-  selectedCategory: string = '';
-  isLoading = false;
 
-  categories = [
-    { name: 'Standard' },
-    { name: 'Deluxe' },
-    { name: 'Suite' }
-  ];
-
-  handleBooking() {
-    if (!this.startDate || !this.endDate || !this.selectedCategory) {
-      alert('Please select all booking options');
-      return;
-    }
-    this.isLoading = true;
-
-    // Simulate booking delay (replace with real API call)
-    setTimeout(() => {
-      this.isLoading = false;
-      alert(`Booked a ${this.selectedCategory} from ${this.startDate} to ${this.endDate}`);
-    }, 2000);
-  }
 }
