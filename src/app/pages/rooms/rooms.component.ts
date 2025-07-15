6a2328bb9ba0c796e1e2df70f304daa83cc2dce7
vinudasenith <ha.vinudas@gmail.com>
2025-07-15 11:32:20 +0530

    add loading and empty state message👍

--- src/app/pages/rooms/rooms.component.ts
+++ src/app/pages/rooms/rooms.component.ts
@@ -1,24 +1,33 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-rooms',
   standalone: true,
-  imports: [CommonModule, HttpClientModule],
+  imports: [CommonModule, HttpClientModule, RouterModule],
   templateUrl: './rooms.component.html',
   styleUrls: ['./rooms.component.css']
 })
 
 export class RoomsComponent implements OnInit {
   rooms: any[] = [];
+  loading: boolean = true;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.http.get<any[]>('http://localhost:8080/api/rooms/all').subscribe({
-      next: (data) => this.rooms = data,
-      error: (err) => console.error('❌ Error fetching rooms:', err)
+      next: (data) => {
+        this.rooms = data;
+        this.loading = false;
+      },
+      error: (err) => {
+        console.error('❌ Error fetching rooms:', err);
+        this.loading = false;
+      }
     });
   }
 }
+
