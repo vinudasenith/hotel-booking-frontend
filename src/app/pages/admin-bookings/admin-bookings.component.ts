ef2bbe6eaff8f6569a7693b8280da3a6d17ed643
vinudasenith <ha.vinudas@gmail.com>
2025-07-15 11:30:33 +0530

    implement admin-booking functionalities👍

--- src/app/pages/admin-bookings/admin-bookings.component.ts
+++ src/app/pages/admin-bookings/admin-bookings.component.ts
@@ -1,13 +1,55 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-admin-bookings',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './admin-bookings.component.html',
-  styleUrl: './admin-bookings.component.css'
+  styleUrls: ['./admin-bookings.component.css']
 })
-export class AdminBookingsComponent {
+export class AdminBookingsComponent implements OnInit {
+  booking: any[] = [];
+  loading = true;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.fetchBookings();
+  }
+
+  fetchBookings() {
+    this.loading = true;
+    this.http.get<any[]>('http://localhost:8080/api/bookings/all').subscribe({
+      next: (data) => {
+        this.booking = data;
+        this.loading = false;
+      },
+      error: (err) => {
+        console.error("❌ Failed to fetch bookings", err);
+        this.loading = false;
+      }
+    });
+  }
+
+  changeStatus(bookingId: string, newStatus: string) {
+    const selectedBooking = this.booking.find(b => b.bookingId === bookingId);
+
+    if (!selectedBooking) {
+      alert('Booking not found');
+      return;
+    }
+
+
+    const updatedBooking = { ...selectedBooking, status: newStatus };
+
+    this.http.put(`http://localhost:8080/api/bookings/${bookingId}`, updatedBooking)
+      .subscribe({
+        next: () => this.fetchBookings(),
+        error: err => alert('❌ Failed to update status')
+      });
+  }
 
 }
