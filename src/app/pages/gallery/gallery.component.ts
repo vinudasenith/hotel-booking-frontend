eb243a72e6156ad3abdf840319efc73e82014a4d
vinudasenith <ha.vinudas@gmail.com>
2025-07-17 16:00:34 +0530

    update routerLink navigation✅

--- src/app/pages/gallery/gallery.component.ts
+++ src/app/pages/gallery/gallery.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-gallery',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './gallery.component.html',
   styleUrl: './gallery.component.css'
 })
