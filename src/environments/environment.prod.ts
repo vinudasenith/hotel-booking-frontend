897c7fb1e4cddc5380e455b91c12873c09f94a9c
vinudasenith <ha.vinudas@gmail.com>
2025-07-26 08:33:13 +0530

    update backend hosted url👍

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://hotel-booking-webapp-a4342eeb6e3e.herokuapp.com/api'
+    apiUrl: 'https://hotel-booking-management-web-c30726af1aaf.herokuapp.com/api'
 };
