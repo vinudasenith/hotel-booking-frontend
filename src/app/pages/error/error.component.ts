e29afcda9b3a2ea5a6f1d9abfc016ecd28986e96
vinudasenith <ha.vinudas@gmail.com>
2025-07-27 13:32:13 +0530

    implement  error page👍

---
+++ src/app/pages/error/error.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-error',
+  imports: [CommonModule],
+  templateUrl: './error.component.html',
+  styleUrl: './error.component.css'
+})
+export class ErrorComponent {
+
+}
